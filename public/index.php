6e12556c71b5b8f32832f54a8f13b248542258cd
Matthew Daly <matthewbdaly@gmail.com>
2018-03-24 23:21:16 +0000

    Added routes

--- public/index.php
+++ public/index.php
@@ -4,9 +4,26 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+$routes = include __DIR__.'/../routes.php';
 
 $request = Request::createFromGlobals();
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-$response = new Response('Hello world');
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    $response = new Response(ob_get_clean());
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
+}
 
 $response->send();
