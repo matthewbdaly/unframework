26fe4b4428f24494910dd55e36785bd99b803f20
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-04 21:27:53 +0100

    Added main route

--- public/index.php
+++ public/index.php
@@ -2,38 +2,35 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 use League\Container\Container;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 define(BASE_DIR, getcwd());
-$routes = include __DIR__.'/../routes.php';
 
 $container = new Container;
 $container->addServiceProvider('App\Providers\CacheProvider');
 $container->addServiceProvider('App\Providers\EventProvider');
 $container->addServiceProvider('App\Providers\LoggerProvider');
 $container->addServiceProvider('App\Providers\TwigProvider');
-$request = Request::createFromGlobals();
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
-$twig = $container->get('Twig_Environment');
 
-try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    $logger = $container->get('Psr\Log\LoggerInterface');
-    $logger->error('Testing');
-    $template = $twig->load(sprintf('%s.html', $_route));
-    $template->display();
-    $response = new Response(ob_get_clean());
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$container->share('response', Zend\Diactoros\Response::class);
+$container->share('request', function () {
+    return Zend\Diactoros\ServerRequestFactory::fromGlobals(
+        $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+    );
+});
 
-$response->send();
+$container->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
+
+$route = new League\Route\RouteCollection($container);
+
+$route->map('GET', '/', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $response->getBody()->write('<h1>Hello, World!</h1>');
+
+    return $response;
+});
+
+$response = $route->dispatch($container->get('request'), $container->get('response'));
+
+$container->get('emitter')->emit($response);
