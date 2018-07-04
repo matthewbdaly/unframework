1eafdc2e30031142df13e0e14669ac4aed2cfb09
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-04 22:10:32 +0100

    Now returning a Twig template

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,11 +4,21 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Twig_Environment;
 
 class HomeController
 {
+    protected $twig;
+
+    public function __construct(Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public function index(ServerRequestInterface $request, ResponseInterface $response)
     {
+        $tpl = $this->twig->load('index.html');
+        $response->getBody()->write($tpl->render());
         return $response;
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,10 +3,14 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use League\Container\Container;
+use League\Container\ReflectionContainer;
 
 define('BASE_DIR', getcwd());
 
 $container = new Container;
+$container->delegate(
+    new ReflectionContainer
+);
 $container->addServiceProvider('App\Providers\CacheProvider');
 $container->addServiceProvider('App\Providers\EventProvider');
 $container->addServiceProvider('App\Providers\LoggerProvider');
