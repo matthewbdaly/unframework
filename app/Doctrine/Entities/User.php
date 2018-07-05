fba595ee062b06c4c6d196adfc478da563f244be
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-05 21:40:37 +0100

    Can now interact with user model

---
+++ app/Doctrine/Entities/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Doctrine\Entities;
+
+/**
+ * @Entity
+ * @Table(name="user")
+ */
+class User
+{
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @Column(type="string",length=200)
+     */
+    private $name;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
