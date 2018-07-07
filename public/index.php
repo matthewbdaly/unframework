08ca54f4760c0efb3195259f69d54c1f2e9f4db5
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-07 20:50:49 +0100

    Moved some code out of the web root

--- public/index.php
+++ public/index.php
@@ -1,22 +1,3 @@
 <?php declare(strict_types = 1);
 
 require_once __DIR__.'/../bootstrap.php';
-
-$container->share('response', Zend\Diactoros\Response::class);
-$container->share('request', function () {
-    return Zend\Diactoros\ServerRequestFactory::fromGlobals(
-        $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
-    );
-});
-
-$container->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
-try {
-    $response = $router->dispatch($container->get('request'), $container->get('response'));
-} catch (\League\Route\Http\Exception\NotFoundException $e) {
-    $twig = $container->get('Twig_Environment');
-    $tpl = $twig->load('404.html');
-    $response = $container->get('response');
-    $response->getBody()->write($tpl->render());
-}
-
-$container->get('emitter')->emit($response);
