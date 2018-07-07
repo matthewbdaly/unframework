fbe53689414e244c0bf82001586254d3a4a7144b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-07 17:49:08 +0100

    Added test for home controller

---
+++ tests/Unit/Controllers/HomeControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Mockery as m;
+use App\Controllers\HomeController;
+
+class HomeControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $renderer = m::mock(new \stdClass);
+        $renderer->shouldReceive('render')->once();
+        $twig = m::mock('Twig_Environment');
+        $twig->shouldReceive('load')->with('index.html')->once()->andReturn($renderer);
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody->write')->once();
+        $controller = new HomeController($twig);
+        $controller->index($request, $response);
+    }
+}
