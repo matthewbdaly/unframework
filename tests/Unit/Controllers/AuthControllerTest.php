5efbdc84ecc1afba072c31e16f9aafc49a9335d1
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-07 17:47:47 +0100

    Added test for auth controller

---
+++ tests/Unit/Controllers/AuthControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Mockery as m;
+use App\Controllers\AuthController;
+
+class AuthControllerTest extends TestCase
+{
+    public function testShow()
+    {
+        $renderer = m::mock(new \stdClass);
+        $renderer->shouldReceive('render')->once();
+        $twig = m::mock('Twig_Environment');
+        $twig->shouldReceive('load')->with('login.html')->once()->andReturn($renderer);
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody->write')->once();
+        $controller = new AuthController($twig);
+        $controller->show($request, $response);
+    }
+}
