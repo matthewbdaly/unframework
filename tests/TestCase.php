9c1cad072847f674911c748a3fc68e6521a57692
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-07 17:28:16 +0100

    Amended base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,12 @@
 
 namespace Tests;
 
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
+    use MockeryPHPUnitIntegration;
+
     public function setUp()
     {
         require __DIR__.'/../bootstrap.php';
