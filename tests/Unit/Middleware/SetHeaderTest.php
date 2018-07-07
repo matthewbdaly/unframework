c7ecf1d52c88d6eff4c9ec2d9adcc486970fbde1
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-07 17:54:20 +0100

    Added test for middleware

---
+++ tests/Unit/Middleware/SetHeaderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use Tests\TestCase;
+use Mockery as m;
+use App\Middleware\SetHeader;
+
+class SetHeaderTest extends TestCase
+{
+    public function testSetHeader()
+    {
+        $request = m::mock('Psr\Http\Message\ServerRequestInterface');
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('withHeader')
+            ->with('X-TEST', 'Testing')
+            ->once()
+            ->andReturn($response);
+        $middleware = new SetHeader;
+        $middleware($request, $response, function () use ($response) {
+            return $response;
+        });
+    }
+}
