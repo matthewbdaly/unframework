4d2d01eda22f689fdcb4d6843337b2c931b4cd8a
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-07-07 19:34:12 +0100

    Handler in place

---
+++ app/Exceptions/LogHandler.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace App\Exceptions;
+
+use Psr\Log\LoggerInterface;
+use Throwable;
+use Whoops\Exception\Inspector;
+use Whoops\RunInterface;
+
+class LogHandler
+{
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function __invoke(Throwable $exception, Inspector $inspector, RunInterface $run)
+    {
+        $this->logger->error($exception->getMessage(), $exception->getTrace());
+    }
+}
---
+++ tests/Unit/Exceptions/LogHandlerTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Unit\Exceptions;
+
+use Tests\TestCase;
+use Mockery as m;
+use App\Exceptions\LogHandler;
+
+class LogHandlerTest extends TestCase
+{
+    public function testHandle()
+    {
+        $logger = m::mock('Psr\Log\LoggerInterface');
+        $logger->shouldReceive('error')->once();
+        $exception = new \Exception;
+        $inspector = m::mock('Whoops\Exception\Inspector');
+        $run = m::mock('Whoops\RunInterface');
+        $handler = new LogHandler($logger);
+        $handler($exception, $inspector, $run);
+    }
+}
