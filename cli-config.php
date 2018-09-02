507f3910bae821362cc5b6ab5dc83048efb41668
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-09-02 16:10:41 +0100

    Fixed Doctrine CLI

--- cli-config.php
+++ cli-config.php
@@ -1,10 +1,16 @@
 <?php declare(strict_types = 1);
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use App\Kernel;
 
-// replace with file to your own project bootstrap
-require_once 'bootstrap.php';
+if (!defined('BASE_DIR')) {
+    define('BASE_DIR', __DIR__);
+}
+$dotenv = new Dotenv\Dotenv(BASE_DIR);
+$dotenv->load();
+
+$app = (new Kernel)->bootstrap();
 
 // replace with mechanism to retrieve EntityManager in your app
-$entityManager = $container->get('Doctrine\ORM\EntityManager');
+$entityManager = $app->getContainer()->get('Doctrine\ORM\EntityManager');
 
 return ConsoleRunner::createHelperSet($entityManager);
