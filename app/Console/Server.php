9dc82137b52ff21b77b083fabd9b8543165cc624
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-09-02 12:40:23 +0100

    Server script in place

---
+++ app/Console/Server.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace App\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Server extends Command
+{
+    protected function configure()
+    {
+        $this->setName('server')
+            ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'Which port should the server run on?')
+            ->setDescription('Runs the development server')
+            ->setHelp('This command runs the development server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!$port = $input->getOption('port')) {
+            $port = 8000;
+        }
+        $output->writeln('Running PHP development server on port '.$port.'...');
+        passthru('php -S localhost:'.$port.' -t '.getcwd().'/public');
+    }
+}
