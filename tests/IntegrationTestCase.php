423b6a9aa9583947a4a147d778e8a071ec1e64f7
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-09-30 21:26:38 +0100

    Expanded the makeRequest method

--- tests/IntegrationTestCase.php
+++ tests/IntegrationTestCase.php
@@ -6,9 +6,9 @@ use Zend\Diactoros\ServerRequest;
 
 class IntegrationTestCase extends TestCase
 {
-    public function makeRequest(string $uri, string $method = 'GET'): IntegrationTestCase
+    public function makeRequest(string $uri, string $method = 'GET', array $params = [], array $body = []): IntegrationTestCase
     {
-        $request = new ServerRequest([], [], $uri, $method);
+        $request = new ServerRequest([], [], $uri, $method, 'php://input', [], [], $params, $body);
         $this->response = $this->app->handle($request);
         return $this;
     }
