9a1f2b95b2db6f5855868dfa8056a88df6aa11c9
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-05-27 14:27:10 +0100

    Amended Doctrine provider
    Now allow for retrieving the Doctrine DBAL connection from the container

--- app/Providers/DoctrineProvider.php
+++ app/Providers/DoctrineProvider.php
@@ -5,26 +5,31 @@ namespace App\Providers;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
 
 class DoctrineProvider extends AbstractServiceProvider
 {
     protected $provides = [
+        'Doctrine\DBAL\Connection',
         'Doctrine\ORM\EntityManager',
     ];
 
     public function register(): void
     {
         // Register items
-        $this->getContainer()
-             ->add('Doctrine\ORM\EntityManager', function () {
-                 $paths = ['app/Doctrine/Entities'];
-                 $isDevMode = false;
-                 $dbParams = array(
-                     'driver' => getenv('DB_TYPE'),
-                     'path' => getenv('DB_PATH'),
-                 );
-                 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
-                 return EntityManager::create($dbParams, $config);
-             });
+        $container = $this->getContainer();
+        $container->add('Doctrine\DBAL\Connection', function () {
+            $dbParams = array(
+                'driver' => getenv('DB_TYPE'),
+                'path' => getenv('DB_PATH'),
+            );
+            return DriverManager::getConnection($dbParams);
+        });
+        $container->add('Doctrine\ORM\EntityManager', function () use ($container) {
+            $paths = ['app/Doctrine/Entities'];
+            $isDevMode = false;
+            $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+            return EntityManager::create($container->get('Doctrine\DBAL\Connection'), $config);
+        });
     }
 }
