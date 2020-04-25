0811a65dfe934ce05d58e149546c694c1b1777f3
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-04-25 16:44:15 +0100

    Tidied up some content

--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 $router->get('/', 'App\Controllers\HomeController::index')
-    ->middleware(new App\Middleware\SetHeader);
+    ->middleware(new App\Middleware\SetHeader());
 
 $router->get('/login', 'App\Controllers\AuthController::show');
